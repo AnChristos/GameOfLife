61623ebf904cc082f34db49fc5a72709c38e1c56
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2018-03-06 16:04:58 +0000

    update examples

--- GameOfLife_Convolve2D_Closure.py
+++ GameOfLife_Convolve2D_Closure.py
@@ -5,21 +5,20 @@ import numpy as np
 from scipy.signal import convolve2d
 
 #This is a generator / closure to evolve N steps
-def evolve(world,steps):
+def evolve(world):
 	#[i-1,j-1], [i-1,j], [i-1,j+1]
 	# [i,j-1 ] ,  ----  , [i,j+1]
 	# [i+1,j-1]  [i+1,j]   [i+1,j+1]      
-	for _ in xrange(steps):
-        	# count the number of neighbours 
-        	neigh = convolve2d(world, np.ones((3, 3)), mode='same', boundary='wrap') - world
-        	# game of life rules
-        	world = np.logical_or(np.logical_and(world, neigh ==2), neigh==3)
-        	world = world.astype(int)
-        	yield world
+        # count the number of neighbours 
+        neigh = convolve2d(world, np.ones((3, 3)), mode='same', boundary='wrap') - world
+        # game of life rules
+        world = np.logical_or(np.logical_and(world, neigh ==2), neigh==3)
+        world = world.astype(int)
+        return  world
 
 if __name__ =='__main__':
 
- 	animation=False
+ 	animation=True
 	if not animation:
 		world = np.loadtxt('GliderGun.txt')
 		for nextWorld in evolve(world,10000):
@@ -31,22 +30,21 @@ if __name__ =='__main__':
 		import matplotlib.pyplot as plt
 		import matplotlib.animation as animation
 		# A closure seems nicer for this 
-		def animateGame(world,inFrames,inInterval):
+		def animateGame(inworld,inFrames,inInterval):
 			#get the artist we will need
    			fig=plt.figure()
 			im=plt.imshow(world,cmap=plt.cm.binary,interpolation='nearest',animated=True)
 			#This will genetate as many frames as requested 	
-    			def animationFrames():    
-        			for i in evolve(world,inFrames):
-            				yield i
 
             		#Here we set the 'artist', needs one input argument
 			# which is what animationFrames yields
-    			def animate(board):
-        			im.set_data(board)
+    			def animate(i):
+				global world
+				world=evolve(world)
+        			im.set_data(world)
         			return (im,)
   
-    			ani = animation.FuncAnimation(fig, animate, frames=animationFrames,interval=inInterval,blit=True)
+    			ani = animation.FuncAnimation(fig, animate, frames=inFrames,interval=inInterval,blit=True)
     			plt.show()
 
 		world = np.loadtxt('GliderGun.txt')
