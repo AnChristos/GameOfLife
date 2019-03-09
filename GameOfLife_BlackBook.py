097f39accafc777d13a53bf2a2262b7e01c9ff8c
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2019-03-09 01:11:49 +0000

    Clean up repository solutions

--- GameOfLife_BlackBook.py
+++ GameOfLife_BlackBook.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 import numpy as np
-# The ideas are taken from the C version at chapter 17
-# https://www.phatcode.net/res/224/files/html/ch17/17-01.html
-#Simple function to encode the rules,this depends on how the evolve expects them
 def GameOfLife(world):
-    #Live or dead is the 0th bit
-    #Since the neighbour info is encoded 
     #if it is alive and does not have 2 or 3 neighbours it must die
     idx_alive_to_die= np.logical_and ( (world&0x01) , np.logical_and( (world>>1)!=2,(world>>1)!=3 ) ).nonzero()
     #if it is dead and has 3 neighbours it becomes alive 
@@ -19,50 +14,69 @@ def HighLife(world):
     idx_dead_to_live= np.logical_and(np.logical_or( (world>>1)==3, (world>>1)==6), (world&0x01)==0).nonzero()	
     return idx_alive_to_die,idx_dead_to_live
 
-#This is a generator  to evolve N steps
 
-def evolve(world,steps,ruleFun=GameOfLife,doWrap=False):
-    """ Each cell encodes at bit :
-    0 : Live or dead
-    1-4 : +=2 for each alive neighbour (so values can be 0-16)
-    5-7 :not used  """
-   
-    rows,cols=world.shape
-   
-    visBeginR=3
-    visEndR=rows+3
-    sizeR=rows+6
-    
-    visBeginC=3
-    visEndC=cols+3
-    sizeC=cols+6
-    
-    if doWrap:
-        visBeginR=0
-        visEndR=rows
-        sizeR=rows
-        visBeginC=0
-        visEndC=cols
-        sizeC=cols
-      
-    #internal world
-    #we can can use the internal convention
-    #and also avoid the wrapping
-    worldInternal=np.zeros((sizeR,sizeC))
-    worldInternal[visBeginR:visEndR,visBeginC:visEndC]=world
-    worldInternal=np.array(worldInternal,dtype=np.int8)
-    nonZero=worldInternal.nonzero() 
+# Here I tried to replicate this one from the "black book"
+# https://www.phatcode.net/res/224/files/html/ch17/17-06.html#Heading8
+# Quite  a bit is still lost to the numpy translation, but hope I got
+# the spirit
 
-    bottomedge=sizeR-1
-    rightedge=sizeC-1
-    
-    #[i-1,j-1], [i-1,j], [i-1,j+1]
-    #[i,j-1 ] ,  ----  , [i,j+1]
-    #[i+1,j-1]  [i+1,j]   [i+1,j+1]
-    relative_i= np.array([-1,-1,-1,0,0,1,1,1])[np.newaxis].T
-    relative_j= np.array([-1,0,1,-1,1,-1,0,1])[np.newaxis].T
+# We have is a 3x3 stencil. 
+# Each cell keeps track of his status and of the status of his neighbours.
+# bit 0 Dead/alive
+# bits 1-5 : Add 2 for each live neighboor
 
-    def neighbours(inputworld,updateIdx,toadd):
+# We see which cells change status and then we update their 8 neighbours
+
+# The original had only wrapping for the edges. Here we create a zone
+# of 3 cells at each side to avoid some of the artifacts in the boundaries.
+# More or less we do not want to touch things that have visible neighbours
+# so we need a bit of buffer space .
+
+class ConwayGame:
+    
+    def __init__(self,world,ruleFun=GameOfLife,doWrap=False):
+        #Each cell encodes at bit :
+        #0 : Live or dead
+        #1-4 : +=2 for each alive neighbour (so values can be 0-16)
+        #5-7 :not used  
+        #[i-1,j-1], [i-1,j], [i-1,j+1]
+        #[i,j-1 ] ,  ----  , [i,j+1]
+        #[i+1,j-1]  [i+1,j]   [i+1,j+1]
+        self.relative_i= np.array([-1,-1,-1,0,0,1,1,1],dtype=np.int8)[np.newaxis].T
+        self.relative_j= np.array([-1,0,1,-1,1,-1,0,1],dtype=np.int8)[np.newaxis].T
+        #The rules 
+        self.ruleFun=ruleFun
+        self.doWrap=doWrap
+
+        #To wrap or not to wrap
+        rows,cols=world.shape
+        self.visBeginR=3
+        self.visEndR=rows+3
+        self.sizeR=rows+6
+        self.visBeginC=3
+        self.visEndC=cols+3
+        self.sizeC=cols+6
+        if doWrap:
+            self.visBeginR=0
+            self.visEndR=rows
+            self.sizeR=rows
+            self.visBeginC=0
+            self.visEndC=cols
+            self.sizeC=cols
+
+        self.bottomedge=self.sizeR-1
+        self.rightedge=self.sizeC-1
+
+        #internal world
+        #we can can use the internal convention
+        #and also avoid the wrapping
+        self.worldInternal=np.zeros((self.sizeR,self.sizeC))
+        self.worldInternal[self.visBeginR:self.visEndR,self.visBeginC:self.visEndC]=world
+        self.worldInternal=np.array(self.worldInternal,dtype=np.int8)
+
+        self.neighbours(self.worldInternal.nonzero() ,2)
+
+    def neighbours(self,updateIdx,toadd):
         """Returns the indices of the 8 Neighbours of cell(i,j)
         Wraps around the right/bottom edge of the world.
         The left/up is covered by the numpy conventions"""
@@ -70,90 +84,74 @@ def evolve(world,steps,ruleFun=GameOfLife,doWrap=False):
         #updateIdx[0/1] are (1 row, N columns where N depends on the indices to update)
         #At the end we have 2 matrices with the indexes
         #of the neighbours to update
-        update_i=relative_i+updateIdx[0]
-        update_j=relative_j+updateIdx[1]
-      
+        update_i=self.relative_i+updateIdx[0]
+        update_j=self.relative_j+updateIdx[1]
+
         #Here numpy array does the negative wrapping
         #We need to add the positive.
-        update_i[update_i>bottomedge]=0
-        update_j[update_j>rightedge]=0
-       
-        
-        #add the proper info to the world
-        np.add.at(inputworld, (update_i,update_j),toadd)
-        return inputworld
+        update_i[update_i>self.bottomedge]=0
+        update_j[update_j>self.rightedge]=0
 
-    def evolveCells(inputworld):
+        #add the proper info to the world
+        np.add.at(self.worldInternal, (update_i,update_j),toadd)
+        pass
+
+    def evolveCells(self):
         """The method that does the actual evolution.
         Only the non-zero cells i.e alive or alive neighbours
         Need to be checked. Update the cell plus its neighbours"""
         #Live or dead is the 0th bit
         #Since the neighbour info is encoded 
-        idx_alive_to_die,idx_dead_to_live=ruleFun(inputworld)
+        idx_alive_to_die,idx_dead_to_live=self.ruleFun(self.worldInternal)
         #Now in the world update the cells to change and the 8 neighbour words
         #All the operations are fixed/encoded at this stage i.e
         #we move all things to the next step
-        #we need to add or subtract 2 for each of the 8 neighbours	
+        #we need to add or subtract 2 for each of the 8 neighbours
         if idx_dead_to_live[0].size !=0 :
-            inputworld[idx_dead_to_live] |= 0x01
-            inputworld=neighbours(inputworld,idx_dead_to_live,2)
+            self.worldInternal[idx_dead_to_live] |= 0x01
+            self.neighbours(idx_dead_to_live,2)
         if idx_alive_to_die[0].size!=0 :
-            inputworld[idx_alive_to_die] &= (~0x01) 
-            inputworld=neighbours(inputworld,idx_alive_to_die,-2)
-        return inputworld
+            self.worldInternal[idx_alive_to_die] &= (~0x01) 
+            self.neighbours(idx_alive_to_die,-2)
 
-    worldInternal=neighbours(worldInternal,nonZero,2)
+        if not self.doWrap:
+            self.worldInternal[0,0:]=0
+            self.worldInternal[0:,0]=0
+            self.worldInternal[0:,self.rightedge]=0;
+            self.worldInternal[self.bottomedge,0:]=0;
+        pass 
 
-    #step using the function set up above
-    for _ in xrange(steps):	        
-        nextworld=evolveCells(worldInternal)
-        #  print "===================================================================="
-        # print worldInternal
+    def getCurrent(self):
+        return self.worldInternal[self.visBeginR:self.visEndR,self.visBeginC:self.visEndC]&0x01
 
-        worldInternal=nextworld
-        if not doWrap:
-            worldInternal[0,0:]=0
-            worldInternal[0:,0]=0
-            worldInternal[0:,rightedge]=0;
-            worldInternal[bottomedge,0:]=0;
-        out=worldInternal[visBeginR:visEndR,visBeginC:visEndC]&0x01	
-        yield out
+
+def animateGame(world,inFrames,inInterval,ruleFun=GameOfLife,doWrap=False):
+    #get the artist we will need
+    fig,ax=plt.subplots()
+    im=plt.imshow(world,cmap=plt.cm.binary,interpolation='nearest',animated=True)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    game=ConwayGame(world,GameOfLife,doWrap)
+    #animate function
+    def animate(frame):
+        game.evolveCells()
+        newWorld=game.getCurrent()
+        im.set_data(newWorld)
+        return (im,)
+
+    anim=animation.FuncAnimation(fig, animate,frames=inFrames,interval=inInterval,blit=True)
+    plt.show()
 
 
 if __name__ =='__main__':
-    animation=True
-    world = np.loadtxt('R-Pantomino.txt')
-    if not animation:
-        for nextWorld in evolve(world,10000,GameOfLife,False):
-            pass
-    else:
-        #Test the animation 
-        import matplotlib
-        matplotlib.use('TKAgg')
-        import matplotlib.pyplot as plt
-        import matplotlib.animation as animation
+    world=np.loadtxt('GliderGun.txt',dtype=np.int8)
+    #Test the animation 
+    import matplotlib
+    matplotlib.use('TKAgg')
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
 
-        def animateGame(world,inFrames,inInterval):
-            #get the artist we will need
-            fig,ax=plt.subplots()
-            im=plt.imshow(world,cmap=plt.cm.binary,interpolation='nearest',animated=True)
-            ax.set_yticklabels([])
-            ax.set_xticklabels([])
-            #Frame Generation 	
-            def animationFrames():
-                yield world
-                for i in evolve(world,inFrames,GameOfLife,False):
-                    yield i
-
-            #animate function
-            def animate(board):
-                im.set_data(board)
-                return (im,)
-
-            ani = animation.FuncAnimation(fig, animate, frames=animationFrames,interval=inInterval,blit=True)
-            plt.show()
-
-        animateGame(world,2000,30)
+    animateGame(world,inFrames=2000,inInterval=30,doWrap=False)
 
 
 
--- produceShape.py
+++ produceShape.py
@@ -1,7 +1,7 @@
 import numpy as np
-X = np.int8(np.zeros((200, 200)))
+X = np.int8(np.zeros((100, 180)))
 
-X[99,100:102]=1
-X[100,99:101]=1
-X[101,100]=1
+X[49,90:92]=1
+X[50,89:91]=1
+X[51,90]=1
 np.savetxt("R-Pantomino.txt",X,fmt='%d')
