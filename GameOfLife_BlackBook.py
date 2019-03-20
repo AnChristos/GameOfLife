eb18898b2b833472fa8f98f4c7ccb695d2bf7032
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2019-03-20 12:57:15 +0000

    Game of Life 2019 version using the black book alg

--- GameOfLife_BlackBook.py
+++ GameOfLife_BlackBook.py
@@ -1,50 +1,74 @@
 #!/usr/bin/env python
 import numpy as np
-#Rule functions
-def GameOfLife(world):
-    #if it is alive and does not have 2 or 3 neighbours it must die
-    idx_alive_to_die= np.where(np.logical_and((world&0x01), np.logical_or( world-5<0,world-7>0)))
-    #if it is dead and has 3 neighbours it becomes alive 
-    idx_dead_to_live= np.where(world==6)    
-    return idx_alive_to_die,idx_dead_to_live
+# There are more Pythonic solutions
 
-
-def HighLife(world):
-    #if it is alive and does not have 2 or 3 neighbours it must die
-    idx_alive_to_die= np.where(np.logical_and((world&0x01), np.logical_or( world-5<0,world-7>0)))
-    #if it is dead and has 3 or 6 neighbours it becomes alive 
-    idx_dead_to_live= np.where(np.logical_or( world==6, world==12))
-    return idx_alive_to_die,idx_dead_to_live
-
-# Here I tried to replicate this one from the "black book"
+# Here I tried to replicate the logic from
 # https://www.phatcode.net/res/224/files/html/ch17/17-06.html#Heading8
-# Quite  a bit is still lost to the numpy translation, but hope I got
-# the spirit
+# Quite  a bit is still lost to the numpy translation.
+ 
+# Each cell keeps track of his status and of the status of his 8 neighbours.
+# It encodes the following :
+# Bit(s)
+# 0  : Live or dead . So 0 or 1
+# 1-4: +=2 for each alive neighbour (so values can be 0-16)
+# 5-7: Not used
+# Examples: 
+# Alive cell with 0 alive neighbours is (int)(1).
+# Alive cell with 2 live neighbours is (int)(5).
+# Dead cell with 3 alive neighbours is (int)(6).
+# Alive cell with 3 alive neighbours is (int)(7).
 
-# We have is a 3x3 stencil. 
-# Each cell keeps track of his status and of the status of his neighbours.
-# bit 0 Dead/alive
-# bits 1-5 : Add 2 for each live neighboor
+# This way we can check a cell without touching its neighbours.
+# - So first see which cells need to change status and change them
+# - Then notify the 8 neighbours of the cells that changed status
 
-# We see which cells change status and then we update their 8 neighbours
+#Currently, parts of the scanning happen twice, 
+#ideally one would like to do them in the same
+#pass (could do it in C/C++ or without numpy)
 
 # The original had only wrapping for the edges. Here we create a zone
 # of 3 cells at each side to avoid some of the artifacts in the boundaries.
 # More or less we do not want to touch things that have visible neighbours
 # so we need a bit of buffer space .
 
-class ConwayGame:
-    
+
+#Rule functions
+def GameOfLife(world):
+    ''' Game of Life rules.Inputs : world. 
+        Returns a tuple with the indices of the cells that change status,
+        in the form (idx_alive_to_die,idx_dead_to_live)'''
+    #if it is alive and does not have 2 or 3 neighbours it must die.
+    #if it is dead and has 3 neighbours it becomes alive 
+    return (np.nonzero(np.logical_and(np.bitwise_and(world,1), 
+                                      np.logical_and(world!=5,world!=7))),
+            np.nonzero(world==6))
+
+def HighLife(world):
+    ''' HighLife rules: Input world. 
+        Returns a tuple with the indices of the cells that change status,
+        in the form (idx_alive_to_die,idx_dead_to_live)'''
+    #if it is alive and does not have 2 or 3 neighbours it must die
+    #if it is dead and has 3 or 6 neighbours it becomes alive 
+    return (np.nonzero(np.logical_and(np.bitwise_and(world,1), 
+                                      np.logical_and(world!=5,world!=7))),  
+            np.nonzero(np.logical_or( world==6, world==12)))
+
+class ConwayGame:   
+    '''Class representing a world evolving with certain rules. 
+       With wrapping or not wrapping of the edges'''
     def __init__(self,world,ruleFun=GameOfLife,doWrap=False):
-        #Each cell encodes at bit :
-        #0 : Live or dead
-        #1-4 : +=2 for each alive neighbour (so values can be 0-16)
-        #5-7 :not used  
+        ''' Create instance, with 
+            an input world, 
+            a function for the rules ,
+            and option to wrap or not wrap around the edges of the world'''
+        
         #[i-1,j-1], [i-1,j], [i-1,j+1]
         #[i,j-1 ] ,  ----  , [i,j+1]
         #[i+1,j-1]  [i+1,j]   [i+1,j+1]
-        self.relative_i= np.array([-1,-1,-1,0,0,1,1,1],dtype=np.int8)[np.newaxis].T
-        self.relative_j= np.array([-1,0,1,-1,1,-1,0,1],dtype=np.int8)[np.newaxis].T
+        # As two 8x1 row vectors. One for the i axis and one for the j axis
+        self.relative_i= np.array([-1,-1,-1,0,0,1,1,1],dtype=np.int8).reshape(8,1)
+        self.relative_j= np.array([-1,0,1,-1,1,-1,0,1],dtype=np.int8).reshape(8,1)
+        
         #The rules 
         self.ruleFun=ruleFun
         self.doWrap=doWrap
@@ -68,53 +92,53 @@ class ConwayGame:
         self.bottomedge=self.sizeR-1
         self.rightedge=self.sizeC-1
 
-        #internal world
-        #we can can use the internal convention
-        #and also avoid the wrapping
+        #internal world representation
         self.worldInternal=np.zeros((self.sizeR,self.sizeC))
-        self.worldInternal[self.visBeginR:self.visEndR,self.visBeginC:self.visEndC]=world
+        self.worldInternal[self.visBeginR:self.visEndR,
+                           self.visBeginC:self.visEndC]=world
         self.worldInternal=np.array(self.worldInternal,dtype=np.int8)
 
+        #Initialise the info for the neighbours of the live cells
         self.neighbours(self.worldInternal.nonzero() ,2)
-
+   
     def neighbours(self,updateIdx,toadd):
-        """Returns the indices of the 8 Neighbours of cell(i,j)
-        Wraps around the right/bottom edge of the world.
-        The left/up is covered by the numpy conventions"""
-        #mesh_i/j are (8 rows, 1 column)  
-        #updateIdx[0/1] are (1 row, N columns where N depends on the indices to update)
-        #At the end we have 2 matrices with the indexes
-        #of the neighbours to update
-        update_i=self.relative_i+updateIdx[0]
-        update_j=self.relative_j+updateIdx[1]
-
-        #Here numpy array does the negative wrapping
-        #We need to add the positive.
+        """Update the neighbours. 
+        updateIdx[0],updateIdx[1] are the i,j indices 
+        of the cells to update the neighbours of. 
+        We find them and add "toadd" to them"""
+        
+        #update_i/update_j are 8x1 column vectors
+        #updateIdx[0],updateIdx[0] are 1xN row vectors
+        #Adding produces two "aligned" 8xN matrices which
+        # we ravel so we have N*8 indices.
+        #of the neighbours to change
+        update_i=(self.relative_i+updateIdx[0]).ravel()
+        update_j=(self.relative_j+updateIdx[1]).ravel()
+        
+        #Here numpy array does the underflow wrapping
+        #We need to just add  the overlow one.
         update_i[update_i>self.bottomedge]=0
         update_j[update_j>self.rightedge]=0
 
         #add the proper info to the world
         np.add.at(self.worldInternal, (update_i,update_j),toadd)
         pass
-
+  
     def evolveCells(self):
-        """The method that does the actual evolution.
-        Only the non-zero cells i.e alive or alive neighbours
-        Need to be checked. Update the cell plus its neighbours"""
-        #Live or dead is the 0th bit
-        #Since the neighbour info is encoded 
+        """The method that does the actual evolution."""
+        
+        #Call the rule function
         idx_alive_to_die,idx_dead_to_live=self.ruleFun(self.worldInternal)
-        #Now in the world update the cells to change and the 8 neighbour words
-        #All the operations are fixed/encoded at this stage i.e
-        #we move all things to the next step
-        #we need to add or subtract 2 for each of the 8 neighbours
+       
+        #Update the cells needing update and their 8 neighbours
         if idx_dead_to_live[0].size !=0 :
             self.worldInternal[idx_dead_to_live] +=1
             self.neighbours(idx_dead_to_live,2)
         if idx_alive_to_die[0].size!=0 :
-            self.worldInternal[idx_alive_to_die] -=1 
+            self.worldInternal[idx_alive_to_die] -=1
             self.neighbours(idx_alive_to_die,-2)
 
+        #If needed 0 the edges    
         if not self.doWrap:
             self.worldInternal[0,0:]=0
             self.worldInternal[0:,0]=0
@@ -123,39 +147,42 @@ class ConwayGame:
         pass 
 
     def getCurrent(self):
-        return self.worldInternal[self.visBeginR:self.visEndR,self.visBeginC:self.visEndC]&0x01
+        '''current state of the evolving world'''
+        return self.worldInternal[self.visBeginR:self.visEndR,
+                                  self.visBeginC:self.visEndC]&0x01
 
 
-def animateGame(world,inFrames,inInterval,ruleFun=GameOfLife,doWrap=False):
-    #get the artist we will need
+def animateGame(inFrames,inInterval,ConwayGame):
+    '''Produce an animation of size inFrames, 
+    with interval between frames inInterval given a ConwayGame class instance'''
     fig,ax=plt.subplots()
-    im=plt.imshow(world,cmap=plt.cm.binary,interpolation='nearest',animated=True)
+    im=plt.imshow(ConwayGame.getCurrent(),cmap=plt.cm.binary,
+                  interpolation='nearest',animated=True)
     ax.set_yticklabels([])
     ax.set_xticklabels([])
-    game=ConwayGame(world,ruleFun,doWrap)
-    class nonlocal:
-        counter=1
     #animate function
     def animate(frame):
-        
-        game.evolveCells()
-        newWorld=game.getCurrent()
+        ConwayGame.evolveCells()
+        newWorld=ConwayGame.getCurrent()
         im.set_data(newWorld)
         return (im,)
 
-    anim=animation.FuncAnimation(fig, animate,frames=inFrames,interval=inInterval,blit=True)
+    
+    anim=animation.FuncAnimation(fig, animate,frames=inFrames,
+                                 interval=inInterval,blit=True)
     plt.show()
 
 
 if __name__ =='__main__':
     world=np.loadtxt('R-Pantomino.txt',dtype=np.int8)
+    game=ConwayGame(world,GameOfLife,False)
     #Test the animation 
     import matplotlib
     matplotlib.use('TKAgg')
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
 
-    animateGame(world,inFrames=2000,inInterval=30,ruleFun=GameOfLife,doWrap=False)
+    animateGame(2000,30,game)
 
 
 
