cdcc68e9ade54317c1b108bb34a91b92297b0bcb
Christos Anastopoulos <christos.anastopoulos@cern.ch>
2022-12-17 03:11:34 +0000

    update

--- GameOfLife_BlackBook.py
+++ GameOfLife_BlackBook.py
@@ -5,7 +5,7 @@ import numpy as np
 # https://www.phatcode.net/res/224/files/html/ch17/17-06.html#Heading8
 # Quite  a bit is still lost to the numpy translation.
 
-# Each cell keeps track of his status and of the status of his 8 neighbours.
+# Each cell keeps track of its status and of the status of his 8 neighbours.
 # It encodes the following :
 # Bit(s)
 # 0  : Live or dead . So 0 or 1
